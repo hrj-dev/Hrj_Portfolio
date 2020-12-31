fb8a6b26397569786c71a64dabfcaa1ae2ebdc36
hrj-dev <indiahrituraj@gmail.com>
2020-12-31 15:43:49 +0530

    Account Login and Logout

--- Hrj_Portfolio/settings.py
+++ Hrj_Portfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'employee',
     'index',
     'contact',
+    'authentication',
 ]
 
 MIDDLEWARE = [
--- Hrj_Portfolio/urls.py
+++ Hrj_Portfolio/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('index.urls')),
 
     path('employee/', include('employee.urls')),
-    path('contact/', include('contact.urls'))
+    path('contact/', include('contact.urls')),
+path('authentication/', include('authentication.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
